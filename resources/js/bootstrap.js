74dd3ad5e0191ec5e2c92181a4eda4d5593a63a7
Sulaiman Sudirman <sulaiman@operatebeyond.com>
2025-12-17 12:48:45 +0800

    fix: Setup Pusher Client Vue

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,10 +4,32 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+const token = window.localStorage.getItem('userToken');
+
+if (token) {
+    window.axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
+
 configureEcho({
     broadcaster: "pusher",
-    // key: import.meta.env.VITE_PUSHER_APP_KEY,
-    // cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    authorizer: (channel, options) => {
+        return {
+            authorize: (socketId, callback) => {
+                axios.post('/api/broadcasting/auth', {
+                    socket_id: socketId,
+                    channel_name: channel.name
+                })
+                .then(response => {
+                    callback(false, response.data);
+                })
+                .catch(error => {
+                    callback(true, error);
+                });
+            }
+        };
+    },
     // forceTLS: true,
     // wsHost: import.meta.env.VITE_PUSHER_HOST,
     // wsPort: import.meta.env.VITE_PUSHER_PORT,
--- routes/channels.php
+++ routes/channels.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('user.{id}', function ($user, $id) {
+    // Check if the authenticated user has permission to listen to this channel
+    return (int) $user->id === (int) $id;
+});
